9650f2cfc058df86be20a4c14cb70bc8b8b9adf5
Spencer Keith <skeith@ecec.com>
2022-03-22 19:39:10 -0500

    Added server message to client

--- src/server/server.js
+++ src/server/server.js
@@ -3,19 +3,26 @@ const express = require('express');
 const path = require('path');
 const ws = require('ws');
 const mysql = require('mysql2');
+const { unescape, escape } = require('querystring');
+
 
 //*****//*****//*****//*****//*****//*****//*****//*****//*****//*****//*****//*****//
 //*****//       imports ▲ ▲ ▲        web sockets setup ▼ ▼ ▼
 //*****//*****//*****//*****//*****//*****//*****//*****//*****//*****//*****//*****//
 
+
 const socketServer = new ws.Server({ noServer: true });
+
 socketServer.on('connection', socket => {
-    socket.on('message', message => console.log(message));
+    socket.on('message', message => console.log(decodeUTF8(message)));
+    socket.send( JSON.stringify( {action: 'stream', payload: true} ) );  //FIX LATER, MESSAGE IS CURRENTLY STATIC
 });
+
 
 //*****//*****//*****//*****//*****//*****//*****//*****//*****//*****//*****//*****//
 //*****//       web sockets setup ▲ ▲ ▲     database connection setup ▼ ▼ ▼
 //*****//*****//*****//*****//*****//*****//*****//*****//*****//*****//*****//*****//
+
 
 const db = mysql.createConnection({
 
@@ -35,27 +42,45 @@ db.connect((err) => {
 
 });
 
+
 //*****//*****//*****//*****//*****//*****//*****//*****//*****//*****//*****//*****//
 //*****//       database connection setup ▲ ▲ ▲           web server setup ▼ ▼ ▼
 //*****//*****//*****//*****//*****//*****//*****//*****//*****//*****//*****//*****//
 
+
+//declare Express app, specify port
 const app = express();
 const port = process.env.PORT || 8080;
 
+//main (default) website path
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 
+
+
+//enable server listening
 const server = app.listen(port, function() {
     console.log('Server started at localhost:' + port);
 });
 
+//enable socket server for Raspberry Pi connection
 server.on('upgrade', (request, socket, head) => {
-
     socketServer.handleUpgrade(request, socket, head, socket => {
-
         socketServer.emit('connection', socket, request);
-
     })
-
 });
+
+
+//*****//*****//*****//*****//*****//*****//*****//*****//*****//*****//*****//*****//
+//*****//       web server setup ▲ ▲ ▲      UTF-8 encode/decode functions ▼ ▼ ▼
+//*****//*****//*****//*****//*****//*****//*****//*****//*****//*****//*****//*****//
+
+
+function encodeUTF8(message) {
+    return unescape(encodeURIComponent(message));
+}
+
+function decodeUTF8(message) {
+    return decodeURIComponent(escape(message));
+}
